b0d28a864f2c182b07566ae5a42b674c2b86847e
John McCutchen <purplegrits@yahoo.com>
2019-10-16 23:35:13 -0400

    made good progress, still need high scores to storage, penalty for wrong answers, message for answer choice, etc.

---
+++ timer.js
@@ -0,0 +1,26 @@
+
+startButton.addEventListener("click", countDown);
+
+function endScreen(){
+    questionContainerElement.classList.add("hide");
+    endContainer.classList.remove("hide");
+    startOverButton.classList.remove("hide");
+    clearHighButton.classList.remove("hide");
+}
+var myInterval= "";
+var timeLeft = 75;
+
+function countDown() {
+    
+    myInterval = setInterval(function() {
+        document.querySelector("#timer").textContent = "Timer: " + timeLeft;
+    
+        if (timeLeft <= 0) {
+            clearInterval(myInterval);
+            endScreen();
+        }
+        else {
+            timeLeft--;
+           }
+        }, 1000);
+}
